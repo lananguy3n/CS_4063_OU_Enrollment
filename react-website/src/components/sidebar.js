65aaad0980a6b1957c568680f6b8aa76fbb683e7
Paul Odhiambo Gariy <Paul.O.Gariy-1@ou.edu>
2024-04-18 23:36:18 -0500

    Added logo

--- react-website/src/components/sidebar.js
+++ react-website/src/components/sidebar.js
@@ -5,7 +5,11 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
     return (
         <div className=" items-center justify-center w-64 h-screen bg-primary flex flex-col">
-            <h1 className="text-white text-2xl font-bold p-8">Sidebar</h1>
+            <div className='flex flex-row items-center justify-center p-4'>
+                <img src="logo_white.png" alt="Logo" className="w-16 h-16 px-1 py-1" />
+                <h1 className='text-white text-2xl px-0 py-2 font-bold'>ENROLL</h1>
+            </div>
+            
             <ul className=" text-white w-10/12 justify-center">
                 <li className="font-medium px-4 py-3 hover:bg-tertiary cursor-pointer h-12" ><Link to="/">Home</Link></li>
                 <li className="font-medium px-4 py-3 hover:bg-tertiary cursor-pointer h-12 "><Link to="/registrationstatus">Registration Status</Link></li>
