35d077bc4c3a183a2a2b3da678368d9ff1f8484e
hadi-fawad <hadi.fawad@ou.edu>
2024-04-26 02:21:06 -0500

    ClassNav Page Embedding Completed

--- react-website/src/pages/ClassNav.js
+++ react-website/src/pages/ClassNav.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Card from '../components/Card';
+import { FaCheckCircle,FaBell} from "react-icons/fa";
+import { GoArrowRight } from "react-icons/go";
+import { HiOutlineQuestionMarkCircle } from "react-icons/hi2";
+import { BsFillGrid3X3GapFill } from "react-icons/bs";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { CgProfile } from "react-icons/cg";
 
 const ClassNav = () => {
     return (
-        <div>ClassNav</div>
+        <div className='flex flex-col'>
+            <div className='flex flex-row p-2 pb-4 justify-between'>
+                <p className='text-4xl font-bold text-black-400 '>Classnav</p>
+                <div className='flex flex-row float-end space-x-8'>
+                    <HiOutlineQuestionMarkCircle className='text-textprimary text-3xl' />
+                    <FaBell className='text-textprimary text-3xl' />
+                    <BsFillGrid3X3GapFill className='text-textprimary text-3xl' />
+                    <CgProfile className='text-textprimary text-3xl' />
+                </div>
+            </div>
+
+            <div>
+                <h1></h1>
+                <iframe
+                    src="https://classnav.ou.edu"
+                    title="erm"
+                    width="100%"
+                    height="800vh"
+                    allow="fullscreen"
+                    className="embedded-iframe" // Add a class name for styling
+                ></iframe>
+            </div>
+        </div>
+
     );
 }
 export default ClassNav;
